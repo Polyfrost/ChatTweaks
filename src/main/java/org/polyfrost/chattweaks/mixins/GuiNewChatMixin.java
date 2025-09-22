8b9521a0ab6aaef301c10738649af4e6ea45a2de
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-09-22 19:43:02 +0200

    chat delay

--- src/main/java/org/polyfrost/chattweaks/mixins/GuiNewChatMixin.java
+++ src/main/java/org/polyfrost/chattweaks/mixins/GuiNewChatMixin.java
@@ -3,11 +3,14 @@ package org.polyfrost.chattweaks.mixins;
 import net.minecraft.client.gui.ChatLine;
 import net.minecraft.client.gui.GuiNewChat;
 import net.minecraft.util.IChatComponent;
+import org.objectweb.asm.Opcodes;
 import org.polyfrost.chattweaks.features.CompactChatHandler;
+import org.polyfrost.chattweaks.features.GuiNewChatHook;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiNewChat.class)
@@ -28,5 +31,24 @@ public class GuiNewChatMixin {
             CompactChatHandler.setChatLine_addToList((ChatLine) chatLine);
         }
         return chatLine;
+    }
+
+    @Inject(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiNewChat;getLineCount()I"))
+    private void patcher$processMessageQueue(int updateCounter, CallbackInfo ci) {
+        GuiNewChatHook.processMessageQueue();
+    }
+
+    @Inject(method = "clearChatMessages", at = @At("HEAD"))
+    private void patcher$clearMessageQueue(CallbackInfo ci) {
+        GuiNewChatHook.messageQueue.clear();
+    }
+
+    @Inject(
+            method = "drawChat",
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 0)),
+            at = @At(value = "JUMP", opcode = Opcodes.IFEQ, ordinal = 0) // Should hopefully match before the if (flag)
+    )
+    private void patcher$drawMessageQueue(CallbackInfo ci) {
+        GuiNewChatHook.drawMessageQueue();
     }
 }
